876801b7f1bc3f518bcf4506e5a20d5e77bb4a18
ranyery <ranyerysc@gmail.com>
2023-05-28 17:08:06 -0300

    feat: add simple counter app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-cypress-example';
+  public counter: number = 0;
+
+  constructor() {}
+
+  public increment(): void {
+    this.counter += 1;
+  }
+
+  public decrement(): void {
+    this.counter -= 1;
+  }
+
+  public reset(): void {
+    this.counter = 0;
+  }
 }
