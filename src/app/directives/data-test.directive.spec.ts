1aba882b6a9db35f2fd7cfea7e756935f024d2ad
ranyery <ranyerysc@gmail.com>
2023-05-28 19:38:56 -0300

    fix: removed specs

--- src/app/directives/data-test.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DataTestDirective } from './data-test.directive';
-
-describe('DataTestDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DataTestDirective();
-    expect(directive).toBeTruthy();
-  });
-});
